4e5e4b47b7eeee8845cd3fd2a525b640ea8cb12b
Marcos Gabriel <marcosgabriel.1383@gmail.com>
2022-10-27 18:11:19 -0300

    criei os exercicios com JavaScript

---
+++ aula12/ex012.js
@@ -0,0 +1,12 @@
+
+var agora = new Date
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+
+if (hora < 12) {
+    console.log('bom dia')
+} else if (hora <= 17) {
+    console.log('boa tarde')
+} else {
+    console.log('boa noite')
+} 
