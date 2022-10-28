acae4c66850b78f5a58b9eb326462c4a62199652
Marcos Gabriel <marcosgabriel.1383@gmail.com>
2022-10-28 18:49:18 -0300

    repetições com FOR

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+
+var contador = 1
+
+
+while (contador <= 10) {
+    console.log(`Contador ${contador}`)
+    contador++
+} 
+
+
+/*
+do {
+    console.log(`testando ${contador}`)
+    contador++
+} while (contador <=5)
+*/
---
+++ aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log('VAI COMEÇAR')
+for (var contador = 1; contador <= 10; contador++){
+    console.log(contador)
+} 
+console.log('FIM.')
